b7509252b6bff4c092c6a7559702e0499a6717ad
coltonbaird1 <fxcolton@icloud.com>
2023-06-12 18:51:18 -0600

    Created function that compares comp vs player choice
    Created function that uses if statements to compare all possible
    combinations and prints a message to console dependant on result

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 function getCompChoice() {
      compChoice = Math.floor(Math.random() * 3);
      if (compChoice == 0) {
-        compChoice = "Rock"
+        compChoice = "rock"
      }
      else if (compChoice == 1) {
-        compChoice = "Paper"
+        compChoice = "paper"
      }
      else {
-        compChoice = "Scissors"
+        compChoice = "scissors"
      }
      return compChoice
 }
@@ -18,3 +18,47 @@ function getPlayerChoice() {
     playerEntry = prompt("Rock, Paper, or Scissors?: ")
     playerChoice = playerEntry.toLowerCase()
 }
+// Initializes score variables
+let playerScore = 0;
+let compScore = 0;
+
+// function that compares comp choice to player choice and returns score and status
+function compareChoices() {
+    if (compChoice == playerChoice) {
+        console.log("Its a tie!")
+    } else if (compChoice == "rock" && playerChoice == "scissors") {
+        console.log("You lost! Rock beats scissors!")
+        compScore++
+        console.log("Computer: " + compScore)
+        console.log("You: " + playerScore)
+    } else if (compChoice == "paper" && playerChoice == "rock") {
+        console.log("You lost! Paper beats rock!")
+        compScore++
+        console.log("Computer: " + compScore)
+        console.log("You: " + playerScore)
+    } else if (compChoice == "scissors" && playerChoice == "paper") {
+        console.log("You lost! Scissors beats paper!")
+        compScore++
+        console.log("Computer: " + compScore)
+        console.log("You: " + playerScore)
+    } else if (playerChoice == "rock" && compChoice == "scissors") {
+        console.log("You won! Rock beats scissors!")
+        playerScore++
+        console.log("Computer: " + compScore)
+        console.log("You: " + playerScore)
+    } else if (playerChoice == "paper" && compChoice == "rock") {
+        console.log("You won! Paper beats rock!")
+        playerScore++
+        console.log("Computer: " + compScore)
+        console.log("You: " + playerScore)
+    } else if (playerChoice == "scissors" && compChoice == "paper") {
+        console.log("You won! Scissors beats paper!")
+        playerScore++
+        console.log("Computer: " + compScore)
+        console.log("You: " + playerScore)
+    }
+}
+
+getCompChoice();
+getPlayerChoice();
+compareChoices();
