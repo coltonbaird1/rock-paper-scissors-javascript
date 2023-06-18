eb176e44417dd10e8977875f728c89154ce81c89
coltonbaird1 <fxcolton@icloud.com>
2023-06-18 14:06:08 -0600

    Added functionality using JS

--- script.js
+++ script.js
@@ -1,75 +1,101 @@
-// // function that randomly selects computers choice
-// function getCompChoice() {
-//      compChoice = Math.floor(Math.random() * 3);
-//      if (compChoice == 0) {
-//         compChoice = "rock"
-//      }
-//      else if (compChoice == 1) {
-//         compChoice = "paper"
-//      }
-//      else {
-//         compChoice = "scissors"
-//      }
-//      return compChoice
-// }
+document.addEventListener("DOMContentLoaded", function() {
+    const playerText = document.querySelector(".player");
+    const compText = document.querySelector(".comp");
+    const resultText = document.querySelector(".result");
+    const compScorePlace = document.querySelector(".compScore");
+    const playerScorePlace = document.querySelector(".playerScore");
+    const myButton = document.querySelectorAll(".my-buttons");
+    const gameButtons = document.getElementById("game-buttons");
+    let playerChoice;
+    let compChoice;
+    let result;
+    let compScore = 0;
+    let playerScore = 0;
 
-// // function that recieves user input and converts it to lowercase
-// // function getPlayerChoice() {
-// //     playerEntry = 
-// //     playerChoice = playerEntry.toLowerCase()
-// // }
-// // Initializes score variables
-// let playerScore = 0;
-// let compScore = 0;
+     //gets comp choice
+     function getCompChoice() {
+        compChoice = Math.floor(Math.random() * 3);
+        if (compChoice == 0) {
+        compChoice = "ROCK"
+        }
+        else if (compChoice == 1) {
+        compChoice = "PAPER"
+        }
+        else {
+        compChoice = "SCISSORS"
+        }
+        return compChoice
+    }
 
-// // function that compares comp choice to player choice and returns score and status
+    //function that compares player and comp choice
+    function compareChoices() {
+        if (compChoice == playerChoice) {
+            resultText.textContent = "It's a tie!";
+        } else if (compChoice == "ROCK" && playerChoice == "SCISSORS") {
+            resultText.textContent = "You lost! Rock beats scissors!";
+            compScore++;
+            compScorePlace.textContent = "Computer Score: " + compScore;
+        } else if (compChoice == "PAPER" && playerChoice == "ROCK") {
+            resultText.textContent = "You lost! Paper beats rock!";
+            compScore++;
+            compScorePlace.textContent = "Computer Score: " + compScore;
+        } else if (compChoice == "SCISSORS" && playerChoice == "PAPER") {
+            resultText.textContent = "You lost! Scissors beats paper!";
+            compScore++;
+            compScorePlace.textContent = "Computer Score: " + compScore;
+        } else if (playerChoice == "ROCK" && compChoice == "SCISSORS") {
+            resultText.textContent = "You Won! Rock beats scissors!";
+            playerScore++;
+            playerScorePlace.textContent = "Player Score: " + playerScore;
+        } else if (playerChoice == "PAPER" && compChoice == "ROCK") {
+            resultText.textContent = "You won! Paper beats Rock!";
+            playerScore++;
+            playerScorePlace.textContent = "Player Score: " + playerScore;
+        } else if (playerChoice == "SCISSORS" && compChoice == "PAPER") {
+            resultText.textContent = "You won! Scissors beats paper!";
+            playerScore++;
+            playerScorePlace.textContent = "Player Score: " + playerScore;
+        }
+    }
+    // Add event listener to the parent container
+    gameButtons.addEventListener('click', handleButtonClick);
+
+    // Event handler function
+    function handleButtonClick(event) {
+    const button = event.target;
+
+    // Check if a button was clicked
+    if (button.tagName === 'BUTTON') {
+        playerChoice = button.getAttribute('data-choice');
+        playerText.textContent = "Players Choice: " + playerChoice;
+        getCompChoice();
+        compText.textContent = "Comp Choice: " + compChoice;
+        compareChoices();
+        
+    }
+    }
+
+   
+})
+
+
+// //function that compares player and comp choice
 // function compareChoices() {
 //     if (compChoice == playerChoice) {
-//         console.log("Its a tie!")
-//     } else if (compChoice == "rock" && playerChoice == "scissors") {
-//         console.log("You lost! Rock beats scissors!")
-//         compScore++
-//         console.log("Computer: " + compScore)
-//         console.log("You: " + playerScore)
-//     } else if (compChoice == "paper" && playerChoice == "rock") {
-//         console.log("You lost! Paper beats rock!")
-//         compScore++
-//         console.log("Computer: " + compScore)
-//         console.log("You: " + playerScore)
-//     } else if (compChoice == "scissors" && playerChoice == "paper") {
+//         resultText.textContent = "It's a tie!";
+//     } else if (compChoice == "ROCK" && playerChoice == "SCISSORS") {
+//         resultText.textContent = "You lost! Rock beats scissors!";
+//     } else if (compChoice == "PAPER" && playerChoice == "ROCK") {
+//         resultText.textContent = "You lost! Paper beats rock!";
+//     } else if (compChoice == "SCISSORS" && playerChoice == "PAPER") {
 //         console.log("You lost! Scissors beats paper!")
-//         compScore++
-//         console.log("Computer: " + compScore)
-//         console.log("You: " + playerScore)
-//     } else if (playerChoice == "rock" && compChoice == "scissors") {
-//         console.log("You won! Rock beats scissors!")
-//         playerScore++
-//         console.log("Computer: " + compScore)
-//         console.log("You: " + playerScore)
-//     } else if (playerChoice == "paper" && compChoice == "rock") {
-//         console.log("You won! Paper beats rock!")
-//         playerScore++
-//         console.log("Computer: " + compScore)
-//         console.log("You: " + playerScore)
-//     } else if (playerChoice == "scissors" && compChoice == "paper") {
+//         resultText.textContent = "You lost! Scissors beats paper!";
+//     } else if (playerChoice == "ROCK" && compChoice == "SCISSORS") {
+//         resultText.textContent = "You Won! Rock beats scissors!";
+//     } else if (playerChoice == "PAPER" && compChoice == "ROCK") {
+//         resultText.textContent = "You won! Paper beats Rock!";
+//     } else if (playerChoice == "SCISSORS" && compChoice == "PAPER") {
 //         console.log("You won! Scissors beats paper!")
-//         playerScore++
-//         console.log("Computer: " + compScore)
-//         console.log("You: " + playerScore)
+//         resultText.textContent = "You won! Scissors beats paper!";
 //     }
 // }
-
-// getCompChoice();
-// getPlayerChoice();
-// compareChoices();
-
-// remove everything below here
-// const buttons = document.querySelectorAll(".myButton");
-
-// buttons.forEach(function(button) {
-//     button.addEventListener('click', function() {
-//         const value = button.dataset.value;
-//         console.log("Button " + value + " clicked!");
-//     });
-
-// });
